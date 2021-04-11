50d647d1d1b73328179c361937af15cc820bfa35
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2021-04-11 17:51:12 +0900

    first commit

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,18 @@
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+
+
+// $(function(){
+//   $(window).scroll(function (){
+//       $('.top_images').each(function(){
+//           var targetElement = $(this).offset().top;
+//           var scroll = $(window).scrollTop();
+//           var windowHeight = $(window).height();
+//           if (scroll > targetElement - windowHeight + 200){
+//               $(this).css('opacity','1');
+//               $(this).css('transform','translateY(0)');
+//           }
+//       });
+//   });
+// });
